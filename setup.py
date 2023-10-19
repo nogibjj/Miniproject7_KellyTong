29ff6ab36303972513690e5c39b87b2c6a67f121
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-19 14:46:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,16 @@ setup(
     author_email="ht117@duke.edu",
     packages=find_packages(),
     install_requires=[
-        "databricks-sql-connector",
+        "black==22.3.0",
+        "click==8.1.3", 
+        "pytest==7.1.3",
+        "pytest-cov==4.0.0",
+        "requests==2.26.0",
+        "ruff==0.0.284",
         "pandas",
         "python-dotenv",
+        "databricks-sql-connector",
+        "setuptools",
     ],
     entry_points={
         "console_scripts": [
